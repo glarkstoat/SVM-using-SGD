96b8e42f9f02edc2a96da4131942aa5fc72ff82f
glarkstoat <74681570+glarkstoat@users.noreply.github.com>
2021-05-04 16:46:05 +0200

    added train/test/split & normalization to Dataloader

--- DataLoader.py
+++ DataLoader.py
@@ -1,33 +1,56 @@
 #%%
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
 
 class DataLoader:
     def __init__(self, path='datasets/'):
         self.path = path
     
-    def get_toydata_tiny(self):
-        """ Returns the features and labels of the tiny toydata set """
+    def get_toydata_tiny(self, train_split_size=0.7, normalize=True):
+        """ Returns the train_test_split of the features and labels of the 
+            tiny toydata set. """
 
         tiny = pd.read_csv(f"{self.path}toydata_tiny.csv", header=0)
         features = tiny[['x1', 'x2']]
         labels = tiny['y']
         
-        return features, labels
+        xtrain, xtest, ytrain, ytest = train_test_split(features, labels, 
+                                            train_size=train_split_size,
+                                            random_state=42) # to keep results consistent
+        if normalize == True:
+            xtrain = StandardScaler().fit_transform(xtrain)
 
-    def get_toydata_large(self):
-        """ Returns the features and labels of the large toydata set """
+        return xtrain, xtest, ytrain, ytest
+
+    def get_toydata_large(self, train_split_size=0.7, normalize=True):
+        """ Returns the train_test_split of the features and labels of the 
+            large toydata set. """
+        
         large = pd.read_csv(f"{self.path}toydata_large.csv", header=0)
         features = large.iloc[:, :8]
         labels = large['y']
         
-        return features, labels
+        xtrain, xtest, ytrain, ytest = train_test_split(features, labels, 
+                                            train_size=train_split_size,
+                                            random_state=42) 
+        if normalize == True:
+            xtrain = StandardScaler().fit_transform(xtrain)
+            
+        return xtrain, xtest, ytrain, ytest
 
-    def get_MNIST(self):
+    def get_MNIST(self, normalize=True):
         """ Returns the training, training-labels, the test and 
-            the test-labels.  """
-        mnist = np.load(f"{self.path}mnist.npz")
+            the test-labels of the MNIST dataset.  """
         
-        return mnist['train'], mnist['train_labels'], mnist['test'], mnist['test_labels']
+        mnist = np.load(f"{self.path}mnist.npz")
+        xtrain = mnist['train']
+        
+        if normalize == True:
+            xtrain = StandardScaler().fit_transform(xtrain)
+        
+        return xtrain, mnist['train_labels'], mnist['test'], mnist['test_labels']
 
 #%%
