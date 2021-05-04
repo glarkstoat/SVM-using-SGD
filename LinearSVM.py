e1beb609cc1f80b0b9efbd140454c7d3f8bb4811
glarkstoat <74681570+glarkstoat@users.noreply.github.com>
2021-05-04 16:57:52 +0200

    Create LinearSVM.py

---
+++ LinearSVM.py
@@ -0,0 +1,18 @@
+from DataLoader import DataLoader
+
+class LinearSVM:
+    """  
+        
+        Parameters
+        ----------
+        
+        """
+        
+    def __init__(self):
+        pass
+
+    def train(self):
+        return self
+
+    def test(self):
+        return
