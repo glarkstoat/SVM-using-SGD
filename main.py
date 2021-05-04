29a80b2abc7a7af48bd6fbed55a9aa69c7bf134d
glarkstoat <74681570+glarkstoat@users.noreply.github.com>
2021-05-04 17:04:24 +0200

    main

---
+++ main.py
@@ -0,0 +1,22 @@
+#%%
+'''
+MMD-Assignment #2: Support Vector Machines using SGD
+    
+Team: Emin Guliev, Justus Rass & Christian Wiskott.
+'''
+
+from LinearSVM import LinearSVM
+import datetime
+
+print("##### Phase-1: #####\n")
+
+
+
+print(" ##### Phase-2:  #####\n")
+
+
+
+print(" ##### Phase-3:  #####\n")
+
+
+# %%
