53deea9d0e638ae16e04b86be401ab81bac76965
ezorrio <ezorrio@gmail.com>
2021-05-05 13:46:08 +0200

    SGD fixes. Added test

--- SGD.py
+++ SGD.py
@@ -1,20 +1,33 @@
 from typing import Tuple
 
+import numpy as np
+
 
 class SGD:
-    def __init__(self, initial_weight, learning_rate):
-        self.weight = initial_weight
+    def __init__(self, learning_rate):
+        self.weight = None
         self.learning_rate = learning_rate
 
     def predict(self, features):
+        if self.weight is None:
+            print("You need to train first!")
+            return None
         result = self.weight[0]
         result += sum(self.weight[i + 1] * features[i] for i in range(len(features) - 1))
         return result
 
-    def train(self, training_data: Tuple[list, list], epoch_count):
+    def train(self, xtrain, ytrain, epoch_count):
+        if len(xtrain) != len(ytrain):
+            print("Check your training data dimensions.")
+        data_count = len(xtrain)
         for epoch in range(epoch_count):
             error = 0
-            for itemx, itemy in training_data:
+            # todo should we iterate over all items??
+            for i in range(data_count):
+                itemx = xtrain[i]
+                itemy = ytrain[i]
+                if self.weight is None:
+                    self.weight = np.zeros(len(itemx) + 1)
                 prediction = self.predict(itemx)
                 # we sum up squared errors
                 current_error = prediction - itemy
