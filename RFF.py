8faa8e4ce52df1c7aa51ecc86371316bb68dc5d3
genkidama420 <a01504276@unet.univie.ac.at>
2021-05-16 15:55:21 +0200

    Extended RFF class with self test function

--- RFF.py
+++ RFF.py
@@ -2,23 +2,48 @@ import numpy as np
 
 
 class RFF:
-    def __init__(self, n_components=100, gamma=1):
+    def __init__(self, n_components=100, sigma=1, delta=None):
         self.n_components = n_components
-        self.gamma = gamma
+        self.sigma = sigma
+        self.delta = delta
         self.weight = None
+
+    def RBF(self):
+        return np.exp(-np.abs(self.delta) ** 2 / (2 * self.sigma ** 2))
+
+
+    def test_approx(self):
+
+        for _ in range(self.n_components):
+            w, b = self.generate(np.zeros_like(self.n_components))
+            self.k_approx = np.zeros_like(self.delta)
+
+            for i, Y in zip(range(len(self.k_approx)), self.delta):
+                x = 0
+                y = -Y
+                zx = np.sqrt(2) / np.sqrt(self.n_components) * np.cos(w*x + b)
+                zy = np.sqrt(2) / np.sqrt(self.n_components) * np.cos(w*y + b)
+                self.k_approx[i] = np.dot(zx, zy.T)
+
 
     def generate(self, X):
         # Generates random weights according to RBF RFF
-        self.weight = 1 / self.gamma * np.random.normal(size=(X.shape[1], self.n_components))
+        try:
+            self.weight = 1 / self.sigma * np.random.normal(size=(X.shape[1], self.n_components))
+        
+        except:
+            self.weight = 1 / self.sigma * np.random.normal(size=(1, self.n_components))
         # Generates random offsets according to RBF RDD
         self.b = np.random.uniform(0, 2 * np.pi, size=self.n_components)
-        return self
+
+        return self.weight, self.b
 
     def transform(self, X):
         # Calculates the transformation for the approx. kernel
         # in multiple steps. Could probably done in one line as well.
         proj = np.dot(X, self.weight)
         proj += self.b
-        np.cos(proj, proj)  # Some sklearn magic
+        np.cos(proj, out = proj) # ensures that in dimension = out dimension
         proj *= np.sqrt(2 / self.n_components)
+
         return proj
